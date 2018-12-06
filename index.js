660613c0f1ff2002d3bda525c7c7a28ffe1eda8e
kenny herillard <Herillard>
2018-12-06 15:07:37 +0100

    Can type help, version or today in settings

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
+const figlet = require('figlet');
 const chalk = require('chalk');
-const prompt = require('prompt');
+const minimist = require('minimist');
 
-prompt.start();
+module.exports = () => {
+    const args = minimist(process.argv.slice(2))
+    console.log(process.argv)
+  }
 
-prompt.get(["folder"], function(err, result) {
-    console.log(chalk.blue(result.folder));
-})
+app = {
+    title: figlet.textSync('Weather Channel', "Ogre"),
+    log_title : function() {
+        console.log(chalk.green(this.title));
+    }
+}
+
+app.log_title();
+const arg = GetArgs();
+
+function GetArgs() {
+    return minimist(process.argv.slice(2));
+}
+
+function CheckSettings(args) {
+    let settings = args._[0] || 'help'
+
+    if (args.version || args.v) {
+      settings = 'version'
+    }
+  
+    if (args.help || args.h) {
+      settings = 'help'
+    }
+
+  switch (settings) {
+    case 'today':
+      console.log("today is sunny");
+      break
+    case 'version':
+      console.log(`v${version}`);
+      break;
+    case 'help':
+      console.log("help");
+      break;
+    default:
+      console.error(`${settings} is not a valid command! type help to have some tips`)
+      break
+  }
+}
+
+CheckSettings(arg);
