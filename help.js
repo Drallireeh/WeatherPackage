6d7872ee040b1c547fc22271028580fc17ec614a
kenny herillard <Herillard>
2018-12-11 09:09:53 +0100

    add few changes in help menu

--- help.js
+++ help.js
@@ -2,22 +2,22 @@ const menus = {
     main: `
         node index.js [command] <options>
   
-        today       ............      show weather for today
-        week       ............      show weather for a week since today
-        help        ............      show help menu for a command`,
+        today       ----------      show weather for today
+        week        ----------      show weather for a week since today
+        help        ----------      show help menu for a command`,
 
     today: `
         node index.js today <options>
   
-        --location, -l .......      the location to use. (ex : "Paris, France")`,
+        --location, -l  ----------      the location to use. (ex : "Paris, Île-de-France")`,
     week: `
         node index.js week <options>
 
-        --location, -l .......      the location to use. (ex : "Paris, France")`,
+        --location, -l  ----------      the location to use. (ex : "Paris, Île-de-France")`,
 };
 
 module.exports = (args) => {
     const sub_settings = args._[0] === 'help' ? args._[1] : args._[0];
 
     console.log(menus[sub_settings] || menus.main);
-};
+};  
